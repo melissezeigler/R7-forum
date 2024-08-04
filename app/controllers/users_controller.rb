66d499440febef72381bf0b66409bf1eafc37839
Melisse Zeigler <melissebernadas@gmail.com>
2024-08-03 21:26:51 -0800

    lesson 7_1 commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  # before_action :set_user, only: %i[ show edit update destroy ]
 
-  # GET /users or /users.json
-  def index
-    @users = User.all
-    if session[:current_user]
-      @current_user = User.find(session[:current_user])
-    else
-      @current_user = nil
-    end
-  end
+  # # GET /users or /users.json
+  # def index
+  #   @users = User.all
+  #   if session[:current_user]
+  #     @current_user = User.find(session[:current_user])
+  #   else
+  #     @current_user = nil
+  #   end
+  # end
 
   # GET /users/1 or /users/1.json
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,27 @@
 Rails.application.routes.draw do
+  get 'posts/create'
+  get 'posts/new'
+  get 'posts/edit'
+  get 'posts/show'
+  get 'posts/update'
+  get 'posts/destroy'
   # resources :users
-  get '/users', to: 'users#index', as: 'users'
-  get '/users/new', to: 'users#new', as: 'new_user'
-  get '/users/:id', to: 'users#show', as: 'user'
-  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
-  post '/users', to: 'users#create'
-  post '/users/:id/logon', to: 'users#logon', as: 'user_logon'
-  delete '/users/logoff', to: 'users#logoff', as: 'user_logoff'
-  patch '/users/:id', to: 'users#update'
-  # delete '/users/:id', to: 'users#delete'
-  resources :forums
+  # root 'forums#index'
+  # get '/users', to: 'users#index', as: 'users'
+  # get '/users/new', to: 'users#new', as: 'new_user'
+  # get '/users/:id', to: 'users#show', as: 'user'
+  # get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  # post '/users', to: 'users#create'
+  # post '/users/:id/logon', to: 'users#logon', as: 'user_logon'
+  # delete '/users/logoff', to: 'users#logoff', as: 'user_logoff'
+  # patch '/users/:id', to: 'users#update'
+  # # delete '/users/:id', to: 'users#delete'
+  # resources :forums
+  resources :forums do
+    resources :posts, shallow: true, except: [:index]
+    resources :subscriptions, shallow: true, except: [:index]
+  end
+  get '/subscriptions', to: 'subscriptions#index', as: 'subscriptions'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
